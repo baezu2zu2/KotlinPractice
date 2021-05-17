dc1f5904f8e2a46b7cb94ce3a7b9cadf683c1fce
baezu2zu2 <baezu2zu2@gmail.com>
2021-05-17 23:33:27 +0900

    weapon name changed

--- src/main/kotlin/item.kt
+++ src/main/kotlin/item.kt
@@ -46,7 +46,7 @@ class MagicWeapon(override val weaponType : WeaponType, override var lvl : Int =
 
 enum class WeaponType(open var atk : Double, open val typeName : String, open val lvlUpNum : Double, val mana : Double = 0.0){
     BAESUHAN_SWORD(1.0, "배수한의 검", 0.75),
-    HAOJUNWAN_ACNE(5.0, "허준원의 여드름",2.1),
+    HAOJUNWAN_ACNE(5.0, "누군가의 여드름",2.1),
     MOTHER_HAND(7.5, "어머니의 손맛", 2.0),
     FOOT_SMELL(4.5, "발냄새", 1.7, 1.0);
 }
